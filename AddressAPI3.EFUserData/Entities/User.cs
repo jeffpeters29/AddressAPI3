4befa5c66b160b31c337defa05013edd03690294
Jeff Peters <jeffpeters@socialicity.co.uk>
2018-09-14 16:04:25 +0100

    DbUser Migrations

---
+++ AddressAPI3.EFUserData/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using AddressAPI3.EFData.Entities;
+
+namespace AddressAPI3.EFUserData.Entities
+{
+    public class User : BaseEntity
+    {
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+    }
+}
---
+++ AddressAPI3.EFUserData/Migrations/20180914144754_InitialMigration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AddressAPI3.EFUserData.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    Username = table.Column<string>(maxLength: 50, nullable: false),
+                    PasswordHash = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FirstName", "LastName", "PasswordHash", "Username" },
+                values: new object[] { 1, "Eddie", "Eagle", "Da21ROPBNsSb3f3CzxgoxWX2YkMmLh8QmqeBiIZ4fXs=", "EE" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FirstName", "LastName", "PasswordHash", "Username" },
+                values: new object[] { 2, "Freddie", "Flintoff", "B9tplMn7jIAnRdZseqRCvzGko0ZHFj7+B1IDQ4u1k/8=", "FF" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FirstName", "LastName", "PasswordHash", "Username" },
+                values: new object[] { 3, "Graham", "Gooch", "/21TLJ9cxwyebxIAz2R92sxLU3beXJ4Jyihjdxsxu0E=", "GG" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
