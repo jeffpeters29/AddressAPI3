ac50482b0640356b61ccb98101395fcce8db8b8c
Jeff Peters <jeffpeters@socialicity.co.uk>
2018-09-19 11:04:47 +0100

    clean up AddressController

--- AddressAPI3.PAF/Controllers/AddressController.cs
+++ AddressAPI3.PAF/Controllers/AddressController.cs
@@ -35,20 +35,21 @@ namespace AddressAPI3.API.Controllers
         [HttpGet("{searchTerm}")]
         public async Task<IActionResult> GetAddressPostcode(string searchTerm)
         {
+            searchTerm = searchTerm.ToUpper();
             var userId = this.User.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")?.Value;
             var uri = this.User.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/uri")?.Value;
             var referer = this.Request.Headers["Referer"].ToString();
 
+            if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(uri) || string.IsNullOrEmpty(referer)) return Unauthorized();
             if (IsUnrecognisedReferer(uri, referer)) return Unauthorized();
 
             try
             {
-                var sw = new Stopwatch();
-                sw.Start();
+                var sw = StartStopwatch();
 
                 var addresses = _addressRepository.GetAddresses(searchTerm);
 
-                await LogActivity(userId, referer, searchTerm.ToUpper(), sw);
+                await LogActivity(userId, referer, searchTerm, sw);
 
                 if (addresses.Any())
                     return Ok(addresses);
@@ -58,9 +59,16 @@ namespace AddressAPI3.API.Controllers
             catch (Exception ex)
             {
                 _logger.LogCritical($"###  Exception whilst searching for postcode {searchTerm} ### ", ex);
-                _mailService.Send($"PAF Critical Error - {searchTerm}", ex.Message);
+                _mailService.Send($"PAF API : Critical Error - {searchTerm}", ex.Message);
                 return StatusCode(500, "A problem happened whilst searching for searchTerm {searchTerm}");
             }
+        }
+
+        private static Stopwatch StartStopwatch()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            return sw;
         }
 
         private async Task LogActivity(string userId, string referer, string searchTerm, Stopwatch sw)
@@ -69,8 +77,10 @@ namespace AddressAPI3.API.Controllers
 
             if (string.IsNullOrEmpty(searchTerm)) return;
 
+            // Simple logging (NLog / Console)
             _logger.LogInformation($"### Postcode searched for {searchTerm}");
 
+            // Log statistics to db
             await _userRepository.LogActivity(new ActivityLog()
             {
                 UserId = Convert.ToInt32(userId),
@@ -78,6 +88,15 @@ namespace AddressAPI3.API.Controllers
                 SearchTerm = searchTerm,
                 ElapsedTime = sw.ElapsedMilliseconds
             });
+
+            // Mail AppErrors
+            if (IsElapsedTimeTooLong(sw))
+                _mailService.Send("PAF API : Time Warning", $"Elapsed Time : {sw.ElapsedMilliseconds}");
+        }
+
+        private static bool IsElapsedTimeTooLong(Stopwatch sw)
+        {
+            return sw.ElapsedMilliseconds > 2000;
         }
 
         private static bool IsUnrecognisedReferer(string claimsUri, string referer)
